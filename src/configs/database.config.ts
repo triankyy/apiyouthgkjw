605682a4e4ad20cf8b1c05ecb960b2a0ce5dc10a
triankyy <triankyy@gmail.com>
2022-10-08 20:44:52 +0700

    change url database to pg

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -3,11 +3,12 @@ import { DataSourceOptions } from 'typeorm';
 
 const databaseConfig: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  username: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  url: process.env.DATABASE_URL,
+  // host: process.env.PG_HOST,
+  // port: Number(process.env.PG_PORT),
+  // username: process.env.PG_USERNAME,
+  // password: process.env.PG_PASSWORD,
+  // database: process.env.PG_DATABASE,
   migrationsTableName: 'migrations',
   entities: ['dist/api/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
